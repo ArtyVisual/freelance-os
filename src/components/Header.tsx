dfbc8b79c2776d6859f2b97f03e72cf6b48fccb1
abbas <abbasvajwana1@gmail.com>
2026-05-02 04:18:25 +0530

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,8 +15,11 @@ export default function Header({ user }: HeaderProps) {
     <div className="flex items-center justify-between px-6 py-3 border-b border-gray-800 bg-gray-900">
 
       <div>
-        <h1 className="pl-10 lg:pl-0 pt-1 text-sm md:text-md lg:text-lg font-semibold text-blue-300 m-0">
+        <h1 className="pl-10 lg:pl-0 pt-1 text-lg font-semibold text-blue-300 m-0 hidden sm:block">
           AI-Powered Freelance Revenue & Risk Management System
+        </h1>
+        <h1 className="pl-10 lg:pl-0 pt-1 text-md font-semibold text-blue-300 m-0 sm:hidden">
+          AI-Powered Freelance
         </h1>
       </div>
 
